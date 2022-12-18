0248274667a3ed7cfe248ecbee15030eda414ae4
asmodann <pernin.mickael1993@gmail.com>
2022-12-18 19:30:39 +0000

    Html customized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ['public/**/*.html'],
+  // safelist: [{ pattern: /./ }],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#88883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
   plugins: [],
-  safelist: [{ pattern: /./ }]
 }
